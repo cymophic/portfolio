c81208d1f54a13f6e83dae7f0f9cf4b20ce2899d
Luis Abhram <luizabhram@gmail.com>
2026-04-08 11:34:09 +0800

    style: Change width to full screen for smaller screen-sizes

--- src/components/layout/nav/NavPanel.tsx
+++ src/components/layout/nav/NavPanel.tsx
@@ -28,7 +28,7 @@ export default function NavPanel({
   return (
     <div
       ref={panelRef}
-      className="fixed top-0 left-0 h-full w-56 z-40 bg-zinc-50 dark:bg-zinc-950 border-r border-zinc-200 dark:border-zinc-800 flex flex-col justify-between py-4 px-6"
+      className="fixed top-0 left-0 h-full w-full md:w-64 z-40 bg-zinc-50 dark:bg-zinc-950 border-r border-zinc-200 dark:border-zinc-800 flex flex-col justify-between py-4 px-6"
     >
       <div className="mt-16">
         <nav className="flex flex-col gap-1.5">
