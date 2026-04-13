d89c8dd8091dc27dfeefe53482997c86e20afc93
Luis Abhram <luizabhram@gmail.com>
2026-04-13 12:14:35 +0800

    feat: Add Footer section to website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,17 @@ import Work from "@/components/sections/Work";
 import Education from "@/components/sections/Education";
 import Projects from "@/components/sections/Projects";
 import TechStack from "@/components/sections/TechStack";
+import Footer from "@/components/sections/Footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col font-sans pt-20 pb-20 sm:pt-28 sm:pb-28 gap-20">
+    <div className="flex flex-1 flex-col font-sans py-18 sm:py-18 gap-20">
       <Intro />
       <Work />
       <Education />
       <Projects />
       <TechStack />
+      <Footer />
     </div>
   );
 }
