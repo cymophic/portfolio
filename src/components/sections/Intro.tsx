1ec92779a4544914a0360e52041d910b07f7d806
Luis Abhram <luizabhram@gmail.com>
2026-04-14 14:50:07 +0800

    feat: Improve position of profile image in small screens and add a greeting above name

--- src/components/sections/Intro.tsx
+++ src/components/sections/Intro.tsx
@@ -5,17 +5,23 @@ import ProfileImage from "@/components/sections/common/ProfileImage";
 export default function Intro() {
   return (
     <section className="w-full">
-      <div className="mx-auto mt-24 flex max-w-4xl flex-col md:flex-row md:items-center md:justify-between md:gap-12 px-6 sm:px-10">
-        <div className="flex flex-col gap-4 md:gap-6">
-          {/* Profile Image - Small Screens */}
-          <div className="shrink-0 md:hidden -ml-1">
-            <ProfileImage width={64} height={64} />
-          </div>
-
+      <div className="flex flex-col mx-auto max-w-4xl mt-24 px-6 sm:flex-row sm:justify-between sm:gap-12 sm:items-center sm:px-10">
+        <div className="flex flex-col gap-4 sm:gap-6">
           {/* Text Content */}
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold leading-tight tracking-tight text-zinc-700 dark:text-zinc-50">
-            {profileInfo.name}
-          </h1>
+          <div className="flex flex-row justify-between gap-x-8">
+            <div>
+              <p className="max-w-md text-sm sm:text-base text-zinc-600 dark:text-zinc-400">
+                {`Hi! I am`}
+              </p>
+              <h1 className="text-4xl sm:text-5xl font-semibold leading-tight tracking-tight text-zinc-700 dark:text-zinc-50">
+                {profileInfo.name}
+              </h1>
+            </div>
+            {/* Profile Image - Small Screens */}
+            <div className="shrink-0 sm:hidden -ml-1">
+              <ProfileImage width={64} height={64} />
+            </div>
+          </div>
           <p className="max-w-md text-sm sm:text-base text-zinc-600 dark:text-zinc-400">
             {profileInfo.bio}
           </p>
@@ -27,7 +33,7 @@ export default function Intro() {
         </div>
 
         {/* Profile Image - Large Screens */}
-        <div className="shrink-0 hidden md:block">
+        <div className="shrink-0 hidden sm:block">
           <ProfileImage width={160} height={160} />
         </div>
       </div>
