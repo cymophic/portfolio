b1ef006f2d09c013ac93922ba5b4432c3732dc96
Luis Abhram <luizabhram@gmail.com>
2026-04-16 16:09:42 +0800

    feat: Integrate `Cal.com` embed for meeting bookings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
 
   return (
     <html lang="en" className={`${inter.variable} ${geistMono.variable} h-full antialiased font-sans`} suppressHydrationWarning>
-      <body className="min-h-full flex flex-col bg-zinc-50 dark:bg-zinc-950">
+      <body className="min-h-full flex flex-col bg-zinc-50 dark:bg-zinc-950" suppressHydrationWarning>
         <ThemeProvider>{content}</ThemeProvider>
       </body>
       
