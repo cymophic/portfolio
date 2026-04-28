27675d539c8e801e60ec39cf2119d619c69610cd
Luis Abhram <luizabhram@gmail.com>
2026-04-28 17:12:57 +0800

    style: Adjust colors of Timeline component and About section dark and light mode text colors

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <section className="w-full">
       <div className="mx-auto flex flex-col gap-10 px-6 sm:px-10">
-        <SectionTitle title="Bio" />
+        <SectionTitle title="About" />
         <div className="text-base flex flex-col gap-6">
           {profileInfo.bio.map((paragraph, index) => (
             <p 
@@ -13,7 +13,7 @@ export default function About() {
               className={
                 index === 0 
                 ? "text-zinc-700 dark:text-zinc-300"
-                : "text-zinc-500 dark:text-zinc-300/80"
+                : "text-zinc-500 dark:text-zinc-500"
               }
             >
               {paragraph}
