ffd7c8fb4a8aeab64fc80e23ff8702741f244bef
Luis Abhram <luizabhram@gmail.com>
2026-05-01 11:05:05 +0800

    fix: Center align text title and description in ProjectCard component

--- src/app/(main)/projects/[slug]/page.tsx
+++ src/app/(main)/projects/[slug]/page.tsx
@@ -12,7 +12,7 @@ export default async function Project({ params }: { params: Promise<{ slug: stri
 
   return (
     <section className="w-full">
-      <div className="mx-auto max-w-4xl flex flex-col gap-6 px-6 sm:px-10">
+      <div className="mx-auto max-w-4xl flex flex-col px-6 sm:px-10 text-center gap-2">
         <h1 className="text-2xl font-semibold text-zinc-700 dark:text-zinc-300">
           {project.title}
         </h1>
