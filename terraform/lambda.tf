7cc16fa5363098eeb0417915cb80e5f0032c49a5
Luis Abhram <luizabhram@gmail.com>
2026-05-07 18:51:08 +0800

    fix: Add API gateway throttling to protect lambda from cost spikes

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -13,6 +13,11 @@ resource "aws_apigatewayv2_stage" "api" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
+
+  default_route_settings {
+    throttling_burst_limit = 5
+    throttling_rate_limit  = 5
+  }
 }
 
 # Shared IAM Role for all Lambdas
