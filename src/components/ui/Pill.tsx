4ea7759912dc3c142a8058081d8ad9b78d53f0c2
Luis Abhram <luizabhram@gmail.com>
2026-05-16 15:31:48 +0800

    feat: Add `className` props to Pill component

--- src/components/ui/Pill.tsx
+++ src/components/ui/Pill.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/lib/utils/cn";
+
 type Props = {
   text: string;
+  className?: string;
 };
 
-export default function Pill({ text }: Props) {
+export default function Pill({ text, className }: Props) {
   return (
-    <span className="inline-flex items-center rounded-full border border-zinc-300 px-2.5 py-0.5 text-xs text-zinc-700 whitespace-nowrap dark:border-zinc-600 dark:text-zinc-300/80">
+    <span
+      className={cn(
+        `inline-flex items-center rounded-full border border-zinc-300 px-2.5 py-0.5 text-xs text-zinc-700 whitespace-nowrap dark:border-zinc-600 dark:text-zinc-300/80`,
+        className,
+      )}
+    >
       {text}
     </span>
   );
