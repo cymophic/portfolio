f7cff68419f60d4c94da60290e63f8526d379b98
Luis Abhram <luizabhram@gmail.com>
2026-05-16 15:33:13 +0800

    style: Set cursor as default in Pill component

--- src/components/ui/Pill.tsx
+++ src/components/ui/Pill.tsx
@@ -9,7 +9,7 @@ export default function Pill({ text, className }: Props) {
   return (
     <span
       className={cn(
-        `inline-flex items-center rounded-full border border-zinc-300 px-2.5 py-0.5 text-xs text-zinc-700 whitespace-nowrap dark:border-zinc-600 dark:text-zinc-300/80`,
+        `inline-flex items-center rounded-full border border-zinc-300 px-2.5 py-0.5 text-xs text-zinc-700 whitespace-nowrap dark:border-zinc-600 dark:text-zinc-300/80 cursor-default`,
         className,
       )}
     >
